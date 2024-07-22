f2ed7b82ccfeede24f22dfe21bd2088de74ec032
hung201200146@lms.utc.edu.vn <hung201200146@lms.utc.edu.vn>
2024-07-22 10:15:13 +0700

    FullUI

--- CustomUI/Form1.Designer.cs
+++ CustomUI/Form1.Designer.cs
@@ -28,32 +28,131 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.cxhToggleButton2 = new CustomUI.CXHUI.CXHToggleButton();
+			this.cxhComboBox1 = new CustomUI.CXHUI.CXHComboBox();
+			this.cxhToggleButton1 = new CustomUI.CXHUI.CXHToggleButton();
+			this.cxhRadioButton1 = new CustomUI.CXHUI.CXHRadioButton();
+			this.cxhButton2 = new CustomUI.CXHUI.CXHButton();
+			this.cxhTextBox1 = new CustomUI.CXHUI.CXHTextBox();
+			this.cxhCircularPictureBox1 = new CustomUI.CXHUI.CXHCircularPictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.cxhCircularPictureBox1)).BeginInit();
 			this.SuspendLayout();
 			// 
-			// cxhToggleButton2
+			// cxhComboBox1
 			// 
-			this.cxhToggleButton2.AutoSize = true;
-			this.cxhToggleButton2.Location = new System.Drawing.Point(448, 163);
-			this.cxhToggleButton2.MinimumSize = new System.Drawing.Size(45, 22);
-			this.cxhToggleButton2.Name = "cxhToggleButton2";
-			this.cxhToggleButton2.OffBackColor = System.Drawing.Color.Gray;
-			this.cxhToggleButton2.OffToggleColor = System.Drawing.Color.Gainsboro;
-			this.cxhToggleButton2.OnBackColor = System.Drawing.Color.Gray;
-			this.cxhToggleButton2.OnToggleColor = System.Drawing.Color.PaleGreen;
-			this.cxhToggleButton2.Size = new System.Drawing.Size(45, 22);
-			this.cxhToggleButton2.SolidStyle = false;
-			this.cxhToggleButton2.TabIndex = 1;
-			this.cxhToggleButton2.UseVisualStyleBackColor = true;
+			this.cxhComboBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+			this.cxhComboBox1.BorderColor = System.Drawing.Color.MediumSlateBlue;
+			this.cxhComboBox1.BorderSize = 1;
+			this.cxhComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDown;
+			this.cxhComboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+			this.cxhComboBox1.ForeColor = System.Drawing.Color.DimGray;
+			this.cxhComboBox1.IconColor = System.Drawing.Color.MediumSlateBlue;
+			this.cxhComboBox1.ListBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(228)))), ((int)(((byte)(245)))));
+			this.cxhComboBox1.ListTextColor = System.Drawing.Color.DimGray;
+			this.cxhComboBox1.Location = new System.Drawing.Point(585, 202);
+			this.cxhComboBox1.MinimumSize = new System.Drawing.Size(200, 30);
+			this.cxhComboBox1.Name = "cxhComboBox1";
+			this.cxhComboBox1.Padding = new System.Windows.Forms.Padding(1);
+			this.cxhComboBox1.Size = new System.Drawing.Size(200, 30);
+			this.cxhComboBox1.TabIndex = 4;
+			this.cxhComboBox1.Texts = "";
+			// 
+			// cxhToggleButton1
+			// 
+			this.cxhToggleButton1.AutoSize = true;
+			this.cxhToggleButton1.Location = new System.Drawing.Point(473, 55);
+			this.cxhToggleButton1.MinimumSize = new System.Drawing.Size(45, 22);
+			this.cxhToggleButton1.Name = "cxhToggleButton1";
+			this.cxhToggleButton1.OffBackColor = System.Drawing.Color.Gray;
+			this.cxhToggleButton1.OffToggleColor = System.Drawing.Color.Gainsboro;
+			this.cxhToggleButton1.OnBackColor = System.Drawing.Color.MediumAquamarine;
+			this.cxhToggleButton1.OnToggleColor = System.Drawing.Color.WhiteSmoke;
+			this.cxhToggleButton1.Size = new System.Drawing.Size(45, 22);
+			this.cxhToggleButton1.TabIndex = 3;
+			this.cxhToggleButton1.UseVisualStyleBackColor = true;
+			// 
+			// cxhRadioButton1
+			// 
+			this.cxhRadioButton1.AutoSize = true;
+			this.cxhRadioButton1.CheckedColor = System.Drawing.Color.MediumSlateBlue;
+			this.cxhRadioButton1.Location = new System.Drawing.Point(186, 289);
+			this.cxhRadioButton1.MinimumSize = new System.Drawing.Size(0, 21);
+			this.cxhRadioButton1.Name = "cxhRadioButton1";
+			this.cxhRadioButton1.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+			this.cxhRadioButton1.Size = new System.Drawing.Size(119, 21);
+			this.cxhRadioButton1.TabIndex = 2;
+			this.cxhRadioButton1.TabStop = true;
+			this.cxhRadioButton1.Text = "cxhRadioButton1";
+			this.cxhRadioButton1.UnCheckedColor = System.Drawing.Color.Gray;
+			this.cxhRadioButton1.UseVisualStyleBackColor = true;
+			// 
+			// cxhButton2
+			// 
+			this.cxhButton2.BackColor = System.Drawing.Color.MediumSlateBlue;
+			this.cxhButton2.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+			this.cxhButton2.BorderColor = System.Drawing.Color.PaleVioletRed;
+			this.cxhButton2.BorderRadius = 79;
+			this.cxhButton2.BorderSize = 0;
+			this.cxhButton2.FlatAppearance.BorderSize = 0;
+			this.cxhButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.cxhButton2.ForeColor = System.Drawing.Color.White;
+			this.cxhButton2.Location = new System.Drawing.Point(376, 394);
+			this.cxhButton2.Name = "cxhButton2";
+			this.cxhButton2.Size = new System.Drawing.Size(232, 79);
+			this.cxhButton2.TabIndex = 1;
+			this.cxhButton2.Text = "cxhButton2";
+			this.cxhButton2.TextColor = System.Drawing.Color.MediumSlateBlue;
+			this.cxhButton2.UseVisualStyleBackColor = false;
+			// 
+			// cxhTextBox1
+			// 
+			this.cxhTextBox1.BorderColor = System.Drawing.Color.MediumSlateBlue;
+			this.cxhTextBox1.BorderFocusColor = System.Drawing.Color.HotPink;
+			this.cxhTextBox1.BorderRadius = 0;
+			this.cxhTextBox1.BorderSize = 2;
+			this.cxhTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.cxhTextBox1.ForeColor = System.Drawing.Color.DimGray;
+			this.cxhTextBox1.Location = new System.Drawing.Point(82, 55);
+			this.cxhTextBox1.Margin = new System.Windows.Forms.Padding(4);
+			this.cxhTextBox1.Multiline = false;
+			this.cxhTextBox1.Name = "cxhTextBox1";
+			this.cxhTextBox1.Padding = new System.Windows.Forms.Padding(7);
+			this.cxhTextBox1.PasswordChar = false;
+			this.cxhTextBox1.PlaceholderColor = System.Drawing.Color.DarkGray;
+			this.cxhTextBox1.PlaceholderText = "";
+			this.cxhTextBox1.Size = new System.Drawing.Size(250, 31);
+			this.cxhTextBox1.TabIndex = 0;
+			this.cxhTextBox1.Texts = "";
+			this.cxhTextBox1.UnderlinedStyle = false;
+			// 
+			// cxhCircularPictureBox1
+			// 
+			this.cxhCircularPictureBox1.BorderCapStyle = System.Drawing.Drawing2D.DashCap.Triangle;
+			this.cxhCircularPictureBox1.BorderColor = System.Drawing.Color.RoyalBlue;
+			this.cxhCircularPictureBox1.BorderColor2 = System.Drawing.Color.HotPink;
+			this.cxhCircularPictureBox1.BorderLineStyle = System.Drawing.Drawing2D.DashStyle.DashDotDot;
+			this.cxhCircularPictureBox1.BorderSize = 5;
+			this.cxhCircularPictureBox1.GradientAngle = 50F;
+			this.cxhCircularPictureBox1.Location = new System.Drawing.Point(641, 238);
+			this.cxhCircularPictureBox1.Name = "cxhCircularPictureBox1";
+			this.cxhCircularPictureBox1.Size = new System.Drawing.Size(253, 253);
+			this.cxhCircularPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.cxhCircularPictureBox1.TabIndex = 5;
+			this.cxhCircularPictureBox1.TabStop = false;
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(1017, 561);
-			this.Controls.Add(this.cxhToggleButton2);
+			this.Controls.Add(this.cxhCircularPictureBox1);
+			this.Controls.Add(this.cxhComboBox1);
+			this.Controls.Add(this.cxhToggleButton1);
+			this.Controls.Add(this.cxhRadioButton1);
+			this.Controls.Add(this.cxhButton2);
+			this.Controls.Add(this.cxhTextBox1);
 			this.Name = "Form1";
 			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.cxhCircularPictureBox1)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -62,7 +161,12 @@
 		#endregion
 
 		private CXHUI.CXHButton cxhButton1;
-		private CXHUI.CXHToggleButton cxhToggleButton2;
+		private CXHUI.CXHTextBox cxhTextBox1;
+		private CXHUI.CXHButton cxhButton2;
+		private CXHUI.CXHRadioButton cxhRadioButton1;
+		private CXHUI.CXHToggleButton cxhToggleButton1;
+		private CXHUI.CXHComboBox cxhComboBox1;
+		private CXHUI.CXHCircularPictureBox cxhCircularPictureBox1;
 	}
 }
 
